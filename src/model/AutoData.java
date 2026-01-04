9d2acd145a08f2f776e5b17eb5830e0ecfccd77b
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-04 16:48:30 -0500

    commit-AutoCounter .

--- src/model/AutoData.java
+++ src/model/AutoData.java
@@ -1,4 +1,12 @@
 package model;
 
 public class AutoData {
+
+    // simulacion tipo numpy array
+    public static String[] AUTOS = {
+            "Sedan", "SUV", "Sedan", "Camioneta",
+            "SUV", "SUV", "Sedan", "Pickup",
+            "Camioneta", "Sedan", "Pickup"
+    };
 }
+
--- src/service/AutoCounter.java
+++ src/service/AutoCounter.java
@@ -1,4 +1,24 @@
 package service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AutoCounter {
+
+    // equivalente a numpy.unique + count
+    public static Map<String, Integer> contarAutos(String[] autos) {
+
+        Map<String, Integer> conteo = new HashMap<>();
+
+        for (String auto : autos) {
+            conteo.put(auto, conteo.getOrDefault(auto, 0) + 1);
+        }
+
+        return conteo;
+    }
+
+    // conteo total ( similar a numpy.size)
+    public static int totalAutos(String[] autos) {
+        return autos.length;    
+    }
 }
